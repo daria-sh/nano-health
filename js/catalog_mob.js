afb7a9208df7d2298d1745ae3ab83b992660933e
daria-sh <daria.shaman@gmail.com>
2018-12-13 21:35:55 +0700

    Code formated

--- js/catalog_mob.js
+++ js/catalog_mob.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
-	$(".menu").click(function() {
-		if ($(window).width() <= 480){
-			$("#menu_mob").toggleClass("hide");
-		}
-	});
+  $('.menu').click(function() {
+    if ($(window).width() <= 480) {
+      $('#menu_mob').toggleClass('hide');
+    }
+  });
 });
-// скрипт для сохранения форматирования меню и каталога при динамическом изменении размера экрана
 $(window).resize(function() {
-	if ($(window).width() <= 480){
-		$(".inner_menu_verh").addClass("hide");
-	}
+  if ($(window).width() <= 480) {
+    $('.inner_menu_verh').addClass('hide');
+  }
 });
-$(document).ready(function() { 
-	$(".catalog").click(function() {
-		if ($(window).width() <= 480){
-			$(".active_mob").toggleClass("hide");
-			$(this).toggleClass("clicked_name");
-			$(".active_mob").toggleClass("clicked_subunits");
-		}
-	});
+$(document).ready(function() {
+  $('.catalog').click(function() {
+    if ($(window).width() <= 480) {
+      $('.active_mob').toggleClass('hide');
+      $(this).toggleClass('clicked_name');
+      $('.active_mob').toggleClass('clicked_subunits');
+    }
+  });
 });
